bf93bc4b99567923e6c19482f9f01086d860bfa0
student <majintha.c@gmail.com>
2020-03-02 19:53:45 +0530

    lab 3

---
+++ app/src/main/java/com/example/lab03/displayMessageActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class displayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+}
